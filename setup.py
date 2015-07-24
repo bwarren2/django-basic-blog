afddba6526512a7093f8d9e1ba51d9e8c0507b7b
Ben Warren <bwarren2@gmail.com>
2015-07-24 17:47:43 -0400

    Publishing artifacts

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-basic-blog',
+    version='0.1',
+    packages=['blog'],
+    include_package_data=True,
+    license='MIT License',  # example license
+    description='A stupid-simple blog app.',
+    long_description=README,
+    url='https://github.com/bwarren2/django-basic-blog',
+    author='Ben Warren',
+    author_email='bwarren2@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
