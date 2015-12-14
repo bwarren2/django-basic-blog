6fad2232723691c0ebf43dcfe24e1185db7a558a
Ben Warren <bwarren2@gmail.com>
2015-12-14 14:44:41 -0500

    Hax

--- test_project/manage.py
+++ test_project/manage.py
@@ -4,6 +4,10 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
+    os.environ.setdefault(
+        "PYTHONPATH",
+        os.path.abspath(os.path.dirname(__file__))
+    )
 
     from django.core.management import execute_from_command_line
 
