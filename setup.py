0b654599e94f0c6632874e94223de68e4e0b94f0
Ben Warren <bwarren2@gmail.com>
2015-12-18 16:22:20 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-basic-blog',
-    version='0.10',
+    version='0.11',
     packages=['blog'],
     install_requires=[
         'markdown',
